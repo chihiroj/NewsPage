cd614e00497ad785ca3b14ab4bd41f2f31aa4129
chihiroj <77557825+chihiroj@users.noreply.github.com>
2025-05-07 16:02:52 +0200

    Add delete and add article javascript

---
+++ add-article/addArticle.js
@@ -0,0 +1,34 @@
+window.onload = (event) => {
+  document.getElementById("add-article-form").addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    const form = event.target;
+    const data = {
+      name: form.name.value,
+      email: form.email.value,
+      title: form.title.value,
+      imgUrl: form.imgUrl.value,
+      contents: form.contents.value,
+    };
+
+    try {
+      const response = await fetch("http://localhost:8080/api/v1/article", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+      });
+
+      if (!response.ok) {
+        const errorText = await response.text();
+        throw new Error(`Request failed: ${response.status} - ${errorText}`);
+      }
+      window.location.replace("/index.html");
+    } catch (error) {
+      console.error("Submission error:", error);
+      alert("Something went wrong. Please try again.");
+    }
+  });
+  
+}
--- script.js
+++ script.js
@@ -17,13 +17,54 @@ window.onload = (event) => {
     console.error('Error: ', error);
   });
 }
+
+let deleteArticle = (id) => {
+  fetch(`http://localhost:8080/api/v1/article/${id}` , {
+    method: 'DELETE'
+  })
+  .then(response => {
+    if(!response.ok){
+      throw new Error('Failed to delete');
+    }
+    deleteArticleFromHtml(id);
+  })
+  .catch(error => {
+    console.error('Error: ', error);
+  });
+}
+
+let deleteArticleFromHtml = (articleId) => {
+  const articleDiv = document.getElementById(articleId);
+  articleDiv.remove();
+}
+
+let createDeleteButton = (contentCol, article) => {
+  const deleteButtonWrapper = document.createElement('div');
+  deleteButtonWrapper.classList.add('delete-button-wrapper');
+  const deleteButton = document.createElement('button');
+  deleteButton.classList.add('delete-button','btn');
+  deleteButton.dataset.id = article.id;
+  deleteButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                      class="bi bi-trash3" viewBox="0 0 16 16">
+                      <path
+                        d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
+                    </svg>`;
+
+  contentCol.appendChild(deleteButtonWrapper);
+  deleteButtonWrapper.appendChild(deleteButton);
+
+  deleteButton.addEventListener('click' , (event) => {
+    deleteArticle(article.id);
+  });
+}
  
 let addReaderStoriesToWebsite = (articleList) => {
-  const localNewsDiv = document.getElementById("local-news");
+  const localNewsDiv = document.getElementById("local-news-wrapper");
 
   articleList.forEach(article => {
     const cardDiv = document.createElement("div");
-    cardDiv.classList.add('card', 'mb-3', 'local-news');
+    cardDiv.classList.add('row', 'card', 'mb-3', 'local-news-article');
+    cardDiv.id = article.id;
 
     const rowDiv = document.createElement('div');
     rowDiv.classList.add('row','g-0');
@@ -39,10 +80,12 @@ let addReaderStoriesToWebsite = (articleList) => {
     imgCol.appendChild(imgElement);
 
     const contentCol = document.createElement('div');
-    contentCol.classList.add('col-md-8');
+    contentCol.classList.add('col-md-10');
 
+    createDeleteButton(contentCol , article);
+    
     const cardBodyDiv = document.createElement('div');
-    cardBodyDiv.classList.add('card-body');
+    cardBodyDiv.classList.add('card-body','local-news-article');
 
     const titleElement = document.createElement('h5');
     titleElement.classList.add('card-title');
@@ -68,7 +111,6 @@ let addReaderStoriesToWebsite = (articleList) => {
     cardDiv.appendChild(rowDiv);
 
     localNewsDiv.appendChild(cardDiv);
-    
   
   });
 
